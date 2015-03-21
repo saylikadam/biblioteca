b3ee9c1b6511a5c8c0ba2639453f097f55700dbd
Ankur Singh <ankurks@thoughtworks.com>
2015-03-21 14:25:08 +0530

    Libery class created

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,6 +1,11 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
+
+
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,19 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ankurks on 3/21/2015.
+ */
+public class Library {
+    List<String> bookList=new ArrayList<String>();
+   {
+       bookList.add("Frindle");
+       bookList.add("Our IceBerg Is Melting");
+       bookList.add("Oh my God");
+       bookList.add("Holiday");
+   }
+
+
+}
